566994c9ccbfe08058abd432a6f090ba4f0b5d31
Rémi Oudin <oudin@crans.org>
2019-02-25 16:18:25 +0100

    Syntax analysis

---
+++ interpreter/syntax_analysis/parser.py
@@ -0,0 +1,363 @@
+# -*- coding:utf8 -*-
+""" SCI - Simple C Interpreter """
+
+from ..lexical_analysis.token_type import XOR_OP, AND_OP, ADD_OP, SUB_OP
+from ..lexical_analysis.token_type import CMP_OP, CMPL_OP, CMPB_OP, TEST
+from ..lexical_analysis.token_type import JL, JG, JGE, JLE, JE, JMP, JMPQ
+from ..lexical_analysis.token_type import POP, POPQ, PUSH, PUSHQ, MOV, MOVL
+from ..lexical_analysis.token_type import CALLQ, HLT, RETQ
+from ..lexical_analysis.token_type import NOPW, NOPL
+from ..lexical_analysis.token_type import REGISTER
+from ..lexical_analysis.token_type import COMMA, DOLLAR, LPAREN, RPAREN, NUMBER, ASTERISK
+from .tree import *
+
+class ProgrammSyntaxError(Exception):
+    """ A syntax error in the assembly program. """
+
+def error(message):
+    """ An error message. """
+    raise ProgrammSyntaxError(message)
+
+class Parser():
+    """ The effective Assembly parser, which relies on the lexer. """
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.current_token_line = []
+        self.current_token = None
+
+    def eat(self, token_type):
+        """ Compare the current token type with the passed token
+        type and if they match then "eat" the current token
+        and assign the next token to the self.current_token,
+        otherwise raise an exception. """
+        if self.current_token.type == token_type and self.current_token_line:
+            self.current_token_line.pop(0)
+            if self.current_token_line:
+                self.current_token = self.current_token_line[0]
+                return True
+            return False
+        error(
+            'Expected token <{}> but found <{}> at line {}.'.format(
+                token_type, self.current_token.type, self.lexer.line
+            )
+        )
+
+    def program(self):
+        """
+        program                     : declarations
+        """
+        root = Program(
+            sections=self.sections(),
+            line=self.lexer.line,
+            prog_counter=0
+        )
+        return root
+
+    def sections(self):
+        """
+        sections                    : section+
+        """
+        sections = []
+
+        for section in self.lexer.sections:
+            sections.append(self.section(section))
+        return sections
+
+    def section(self, section):
+        """
+        section                     : NUM ID operations+
+        """
+        num = section.start_addr
+        name = section.name
+        content = self.operations(section.operations)
+        return Section(
+            name=name,
+            prog_counter=int(num.value, 16),
+            content=content,
+            line=section.file_line,
+        )
+
+    def operations(self, operations):
+        """
+        operations                  : operation+
+        """
+        result = []
+        for operation in operations:
+            line = operation.line
+            prog_counter = int(operation.pc.value, 16)
+            self.current_token_line = operation.tokens[1:]
+            result.append(self.operation(prog_counter=prog_counter, line=line))
+        return result
+
+    def operation(self, prog_counter, line):
+        """
+        operation                   : operator addr_expression{,2}
+        """
+        self.current_token = self.current_token_line[0]
+        if self.current_token.type is CALLQ:
+            return self.callqop(prog_counter, line)
+        if self.current_token.type in [SUB_OP, XOR_OP, AND_OP, ADD_OP]:
+            return self.binop(prog_counter, line)
+        if self.current_token.type in [JL, JG, JGE, JLE, JE, JMP, JMPQ]:
+            return self.jmpop(prog_counter, line)
+        if self.current_token.type in [CMP_OP, CMPL_OP, CMPB_OP, TEST]:
+            return self.cmpop(prog_counter, line)
+        if self.current_token.type in [POP, POPQ, PUSH, PUSHQ]:
+            return self.stackop(prog_counter, line)
+        if self.current_token.type in [MOV, MOVL]:
+            return self.movop(prog_counter, line)
+        if self.current_token.type in [NOPW, NOPL]:
+            return self.noop(prog_counter, line)
+        if self.current_token.type is HLT:
+            return self.hltop(prog_counter, line)
+        if self.current_token.type is RETQ:
+            return self.retqop(prog_counter, line)
+        error("Unkown operation {} at line {}"
+              .format(self.current_token, line)
+              )
+
+
+    def callqop(self, prog_counter, line):
+        """
+        callqop                     : CALLQ ADDR
+        """
+        operation = self.current_token
+        self.eat(operation.type)
+        if self.current_token_line:
+            call_addr = self.addr_expression(prog_counter, line)
+            if self.current_token.type is COMMA:
+                error("incompatible operand with callq operator at line {}"
+                      .format(line))
+        else:
+            error("incompatible operand with callq operator at line {}"
+                  .format(self.lexer.line))
+        return CallQOp(
+            call_addr=call_addr,
+            ret_addr=str(int(prog_counter, 16)+0x8),
+            prog_counter=prog_counter,
+            line=line
+        )
+
+
+
+    def binop(self, prog_counter, line):
+        """
+        binqop                      : BINOP ADDR COMMA ADDR
+        """
+        operation = self.current_token
+        self.eat(operation.type)
+        left = self.addr_expression(prog_counter, line)
+        if self.current_token.type is COMMA:
+            self.eat(COMMA)
+        else:
+            error("Incompatible Operand {} with binary operator {} at line{}"
+                  .format(left, operation.value, line)
+                  )
+        return BinOp(
+            left=left,
+            op=operation,
+            right=self.addr_expression(prog_counter, line),
+            prog_counter=prog_counter,
+            line=line
+        )
+
+    def jmpop(self, prog_counter, line):
+        """
+        jmpop                       : JMPOP ADDR
+        """
+        operation = self.current_token
+        self.eat(operation.type)
+        addr = self.addr_expression(prog_counter, line)
+        if self.current_token.type is COMMA:
+            error("Incompatible operand with jump operator {} at line{}"
+                  .format(operation.value, line)
+                  )
+        return JmpStmt(
+            op=operation,
+            jmpaddr=addr,
+            line=line,
+            prog_counter=prog_counter
+        )
+
+    def cmpop(self, prog_counter, line):
+        """
+        cmpop                       : CMPOP ADDR COMMA ADDR
+        """
+        operation = self.current_token
+        self.eat(operation.type)
+        left = self.addr_expression(prog_counter, line)
+        if self.current_token.type is COMMA:
+            self.eat(COMMA)
+        else:
+            error("Incompatible operands with binary operator {} at line{}"
+                  .format(operation.value, line)
+                  )
+        return CmpOp(
+            op=operation,
+            left=left,
+            right=self.addr_expression(prog_counter, line),
+            line=line,
+            prog_counter=prog_counter
+        )
+
+    def stackop(self, prog_counter, line):
+        """
+        stackop                     : STACKOP ADDR
+        """
+        operation = self.current_token
+        self.eat(operation.type)
+        addr = self.addr_expression(prog_counter, line)
+        if self.current_token.type is COMMA:
+            error("Incompatible operand with stack operator {} at line{}"
+                  .format(operation.value, line)
+                  )
+        return StackOp(
+            op=operation,
+            expr=addr,
+            line=line,
+            prog_counter=prog_counter
+        )
+
+    def movop(self, prog_counter, line):
+        """
+        movop                       : MOVOP ADDR COMMA ADDR
+        """
+        operation = self.current_token
+        self.eat(operation.type)
+        left = self.addr_expression(prog_counter, line)
+        if self.current_token.type is COMMA:
+            self.eat(COMMA)
+        else:
+            error("Incompatible operand with operator {} at line {}:{}"
+                  .format(operation.value, line, self.current_token.value)
+                  )
+        return MovOp(
+            left=left,
+            op=operation,
+            right=self.addr_expression(prog_counter, line),
+            prog_counter=prog_counter,
+            line=line
+        )
+
+    def noop(self, prog_counter, line):
+        """
+        noop                        : NOP
+        """
+        operation = self.current_token
+        self.eat(operation.type)
+        if self.current_token_line:
+            _ = self.addr_expression(prog_counter, line)
+        return NullOp(
+            op=operation,
+            line=line,
+            prog_counter=prog_counter
+        )
+
+    def hltop(self, prog_counter, line):
+        """
+        hltop                       : HLT
+        """
+        operation = self.current_token
+        res = self.eat(operation.type)
+        if not res:
+            _ = self.addr_expression(prog_counter, line)
+        return NullOp(
+            op=operation,
+            prog_counter=prog_counter,
+            line=line,
+        )
+
+
+    def retqop(self, prog_counter, line):
+        """
+        retqop                      : RETQ
+        """
+        operation = self.current_token
+        self.eat(operation.type)
+        if self.current_token_line:
+            _ = self.addr_expression(prog_counter, line)
+        return NullOp(
+            op=operation,
+            prog_counter=prog_counter,
+            line=line,
+        )
+
+    def addr_expression(self, prog_counter, line):
+        """
+        addr_exp                    : <HARD STUFF>
+        """
+        if self.current_token.type is DOLLAR:
+            self.eat(DOLLAR)
+            if self.current_token.type is NUMBER:
+                token = self.current_token
+                self.eat(NUMBER)
+                return AddrExpression(token, prog_counter, line)
+            error("Invalid offset at line %s" % line)
+        if self.current_token.type is REGISTER:
+            token = self.current_token
+            self.eat(REGISTER)
+            return Register(token, prog_counter, line)
+        if self.current_token.type is NUMBER:
+            token = self.current_token
+            self.eat(NUMBER)
+            if self.current_token.type is LPAREN:
+                self.eat(LPAREN)
+                register = self.addr_expression(prog_counter, line)
+                if self.current_token.type is COMMA:
+                    self.eat(COMMA)
+                    second_reg = self.addr_expression(prog_counter, line)
+                    if self.current_token.type is COMMA:
+                        self.eat(COMMA)
+                        number = self.current_token
+                        self.eat(NUMBER)
+                        self.eat(RPAREN)
+                        return TernaryAddrExpression(
+                            token=token,
+                            reg_1=register,
+                            reg_2=second_reg,
+                            offset=number,
+                            prog_counter=prog_counter,
+                            line=line
+                        )
+                    error("Wrong compound expression")
+                self.eat(RPAREN)
+                return CompoundAddrExpression(
+                    token,
+                    AddrExpression(token, prog_counter, line),
+                    register,
+                    prog_counter,
+                    line
+                )
+            return AddrExpression(token, prog_counter, line)
+        if self.current_token.type is ASTERISK:
+            token = self.current_token
+            self.eat(ASTERISK)
+            compound = self.addr_expression(prog_counter, line)
+            return CompoundAddrExpression(
+                token,
+                AddrExpression(token.value, prog_counter, line),
+                compound,
+                prog_counter,
+                line
+            )
+
+
+
+    def parse(self):
+        """
+        program                     : declarations
+
+        declarations                : declaration operations+
+
+        declaration                 : NUMBER ID
+
+        operations                  : operation | stmt
+
+        operation                   : unop | binop | nullop | noop | stackop | functioncall
+
+        stmt                        : jmpstmt | retstmt
+
+        """
+        node = self.program()
+
+        return node
---
+++ interpreter/syntax_analysis/tree.py
@@ -0,0 +1,119 @@
+# -*- coding:utf8 -*-
+import sys
+
+class Node(object):
+    def __init__(self, prog_counter, line):
+        self.line = line
+        self.prog_counter = prog_counter
+
+
+class NoOp(Node):
+    pass
+
+class Section(Node):
+    def __init__(self, name, prog_counter, content, line):
+        Node.__init__(self, prog_counter, line)
+        self.prog_counter = prog_counter # A struct name
+        self.name = name
+        self.content = content
+
+class AddrExpression(Node):
+    def __init__(self, token, prog_counter, line):
+        Node.__init__(self, prog_counter, line)
+        self.token = token
+        self.value = token.value
+
+class TernaryAddrExpression(Node):
+    def __init__(self, token, reg_1, reg_2, offset, prog_counter, line):
+        Node.__init__(self, prog_counter, line)
+        self.token = token
+        self.reg_1, self.reg_2 = reg_1, reg_2
+        self.offset = offset
+        self.value = token.value
+
+class CompoundAddrExpression(Node):
+    def __init__(self, token, offset, register, prog_counter, line):
+        Node.__init__(self, prog_counter, line)
+        self.token = token
+        self.offset = offset
+        self.register = register
+
+class Register(Node):
+    def __init__(self, token, prog_counter, line):
+        Node.__init__(self, prog_counter, line)
+        self.token = token
+        self.value = token.value
+
+class BinOp(Node):
+    def __init__(self, left, op, right, prog_counter, line):
+        Node.__init__(self, prog_counter, line)
+        self.left = left
+        self.token = self.op = op
+        self.right = right
+
+class MovOp(Node):
+    def __init__(self, left, op, right, prog_counter, line):
+        Node.__init__(self, prog_counter, line)
+        self.left = left
+        self.token = self.op = op
+        self.right = right
+
+class CmpOp(Node):
+    def __init__(self, left, op, right, prog_counter, line):
+        Node.__init__(self, prog_counter, line)
+        self.left = left
+        self.token = self.op = op
+        self.right = right
+
+class NullOp(Node):
+    def __init__(self, op, prog_counter, line):
+        Node.__init__(self, prog_counter, line)
+        self.token = self.op = op
+
+class StackOp(Node):
+    def __init__(self, op, expr, prog_counter, line):
+        Node.__init__(self, prog_counter, line)
+        self.token = self.op = op
+        self.expr = expr
+
+class CallQOp(Node):
+    def __init__(self, call_addr, ret_addr, prog_counter, line):
+        Node.__init__(self, prog_counter, line)
+        self.call_addr = call_addr
+
+
+class JmpStmt(Node):
+    def __init__(self, op, jmpaddr, prog_counter, line):
+        Node.__init__(self, prog_counter, line)
+        self.token = self.op = op
+        self.jmpaddr = jmpaddr
+        self.line = line
+
+class RetStmt(Node):
+    def __init__(self, prog_counter, line):
+        Node.__init__(self, prog_counter, line)
+        self.prog_counter= prog_counter
+        self.line = line
+
+class Program(Node):
+    def __init__(self, sections, prog_counter, line):
+        Node.__init__(self, prog_counter, line)
+        self.children = sections
+
+
+###############################################################################
+#                                                                             #
+#  AST visitors (walkers)                                                     #
+#                                                                             #
+###############################################################################
+
+class NodeVisitor(object):
+    def visit(self, node, dtype=None):
+        #sys.stderr.write("%s\n" % self.__dict__)
+        method_name = 'visit_' + type(node).__name__
+        visitor = getattr(self, method_name, self.generic_visit)
+        return visitor(node)
+
+    def generic_visit(self, node):
+        raise Exception('No visit_{} method'.format(type(node).__name__))
+

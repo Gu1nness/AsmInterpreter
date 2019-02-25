508066a1bcede99c0eaeedb1543311c8b8e40d1f
Rémi Oudin <oudin@crans.org>
2019-02-25 16:10:32 +0100

    Utility attributes for jmps and frames

--- interpreter/interpreter/interpreter.py
+++ interpreter/interpreter/interpreter.py
@@ -15,6 +15,8 @@ class Interpreter(NodeVisitor):
 
     def __init__(self):
         self.memory = Memory()
+        self.frame = None
+        self.jmpd = False
 
     def preload_functions(self, tree):
         for child in tree.children:
@@ -63,15 +65,77 @@ class Interpreter(NodeVisitor):
                 fct_frame = self.memory.ranges[(start,end)]
                 break
         if node.op.type == JG and self.cmp_reg == 2:
-            self.visit(fct_frame, start=addr)
+            self.jmpd = True
+            self.frame = frame
+            return
         if node.op.type == JGE and self.cmp_reg == 1:
-            self.visit(fct_frame, start=addr)
+            self.jmpd = True
+            self.frame = frame
+            return
         if node.op.type == JE and self.cmp_reg == 0:
-            self.visit(fct_frame, start=addr)
+            self.jmpd = True
+            self.frame = frame
+            return
         if node.op.type == JLE and self.cmp_reg == -1:
-            self.visit(fct_frame, start=addr)
+            self.jmpd = True
+            self.frame = frame
+            return
         if node.op.type == JL and self.cmp_reg == -2:
-            self.visit(fct_frame, start=addr)
+            self.jmpd = True
+            self.frame = frame
+            return
+        if node.op.type == JMPQ:
+            self.jmpd = True
+            self.frame = frame
+            return
+
+
+    def visit_CmpOp(self, node):
+        node.right.pointer = False
+        node.left.pointer = True
+        left = self.visit(node.left)
+        right = self.visit(node.right)
+        self.cmp_reg = Number.__cmp__(right, left)
+
+    def visit_CallQOp(self, node):
+        addr = self.call_addr
+        for (start, end) in self.memory.ranges.keys():
+            if start <= addr and addr <= end:
+                fct_frame = self.memory.ranges[(start,end)]
+                break
+        self.visit(fct_frame)
+
+    def visit_RetStmt(self, node):
+        ret_addr = self.memory.registers['rbp']
+        if ret_addr == 0:
+            raise EndOfExecution
+        for (start, end) in self.memory.ranges.keys():
+            if start <= addr and addr <= end:
+                fct_frame = self.memory.ranges[(start,end)]
+                break
+        return self.visit(fct_frame, start=ret_addr)
+
+    def visit_NullOp(self, node):
+        return
+
+    def visit_AddrExpression(self, node):
+        return Number('l', node.value)
+
+    def visit_TernaryAddrExpression(self, node):
+        return self.visit(reg_1) + self.visit(offset) * self.visit(reg_2)
+
+    def visit_CompoundAddrExpression(self, node):
+        if node.token.type == NUMBER:
+            if node.pointer:
+                addr = self.visit(node.offset) + self.visit(node.register)
+                res = self.memory.stack[self.visit(node.offset) + self.visit(node.register)]
+                return Number(node.register.value[0], res)
+            else:
+                return self.visit(node.offset) + self.visit(node.register)
+        if node.token.type == ASTERISK:
+            return self.memory.stack[self.visit(node.register)]
+
+
 
     def interpret(self, tree):
         self.preload_functions(tree)
